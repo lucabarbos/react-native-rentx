f613dde6a4f1b807e95ed8ae90c3bf95c5654264
Lucas Barbosa <contato@lucasbarbosa.dev>
2022-11-10 06:45:55 -0300

    Configuração de tema global

--- App.tsx
+++ App.tsx
@@ -1,7 +1,50 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useCallback, useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { ThemeProvider } from "styled-components/native";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+
 import { Home } from "./src/screens/Home";
 
+import theme from "./src/styles/theme";
+
 export default function App() {
-  return <Home />;
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  useEffect(() => {
+    async function prepare() {
+      await SplashScreen.preventAutoHideAsync();
+    }
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 }
